bd21306c903863f55dfa12bef98e0babb4cbb9cd
pietraspawel <pawel.z.pietras@gmail.com>
2020-11-07 13:31:52 +0100

    add class Configurator

---
+++ src/Configurator.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace pietras;
+
+class Configurator
+{
+    private $configuration;
+
+    /**
+    * Funkcja ładuje konfigurację z pliku $filename do konfiguracji o nazwie $index.
+    */
+    public function loadConfiguration(string $index, string $filename): array
+    {
+        $conf = json_decode(file_get_contents($filename), true);
+        $this->configuration[$index] = $conf;
+        return $conf;
+    }
+
+    /**
+    * Zwraca konfigurację o nazwie $index.
+    */
+    public function getConfiguration(string $index): array
+    {
+        return $this->configuration[$index];
+    }
+
+    /**
+    * Konfigurację o nazwie $index przypisuje do zmiennych globalnych o nazwach takich jak ich klucze.
+    */
+    public function toGlobal(string $index): void
+    {
+        foreach ($this->configuration[$index] as $key => $value) {
+            $GLOBALS[$key] = $value;
+        }
+    }
+}
---
+++ tests/ConfiguratorTest.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace pietras;
+
+use PHPUnit\Framework\TestCase;
+
+class ConfiguratorTest extends TestCase
+{
+    protected $conf1;
+
+    protected function setUp()
+    {
+        $this->conf1 = new Configurator();
+        $this->conf1->loadConfiguration("konfiguracja1", __DIR__ . "/configuration1.json");
+    }
+
+    public function testLoadConfiguration()
+    {
+        $expected = [
+            "wartość1" => 123,
+            "wartość2" => "Zażółć gęślą jaźń"
+        ];
+        $this->assertEquals(
+            $expected,
+            $this->conf1->loadConfiguration("konfiguracja1", __DIR__ . "/configuration1.json")
+        );
+        $this->assertEquals($expected, $this->conf1->getConfiguration("konfiguracja1"));
+    }
+
+    public function testToGlobal()
+    {
+        $this->conf1->toGlobal("konfiguracja1");
+        $this->assertTrue($GLOBALS["wartość1"] == 123);
+        $this->assertTrue($GLOBALS["wartość2"] == "Zażółć gęślą jaźń");
+    }
+}
