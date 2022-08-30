fbf3f1682870fe371293bab84d6736a05ce04a2f
pietraspawel <pawel.z.pietras@gmail.com>
2022-08-30 09:55:37 +0200

    add Controller.php

---
+++ src/Controller.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace pietras\basic;
+
+/**
+ * Klasa abstrakcyjna dla kontrolerów.
+ */
+abstract class Controller
+{
+    /**
+     * @var Application $application Przechowuje obiekt aplikacji.
+     */
+    protected $application;
+
+    public function __construct(\pietras\Application $application)
+    {
+        $this->application = $application;
+    }
+    /**
+     * Tu zaczyna się obsługa danej routy.
+     */
+    abstract public function handle();
+}
