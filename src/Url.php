97662bda95515ab0188cbb029661f62cf32c687a
Paweł Pietras <pawel.z.pietras@gmail.com>
2026-07-29 15:59:53 +0200

    ch src/Url

--- src/Url.php
+++ src/Url.php
@@ -3,72 +3,114 @@
 namespace pietras\basic;
 
 /**
- * Provide class Url.
+ * Represents current request URL.
  */
 class Url
 {
+    // Dla URL: https://abc.def/project/ghi/jkl?mno=123
+    // app url, np: https://abc.def/project
+    private string $appUrl;
+    // base url = /project
+    private string $baseUrl;
+    // full url: https://abc.def/project/ghi/jkl?mno=123
+    private string $fullUrl;
+    // uri = /ghi/jkl?mno=123
+    private string $uri;
+    // path = /ghi/jkl
+    private string $path;
+    // params[0] = "ghi", params[1] = "jkl"
+    private array $params;
 
-    /**
-     * @var string $baseUrl Root.
-     */
-    private $baseUrl;
-    /**
-     * @var array List of url parameters.
-     */
-    private $param;
-
-    /**
-     * Class constructor.
-     */
-    public function __construct(string $baseUrl = "/")
+    public function __construct(string $appUrl)
     {
-        $this->baseUrl = $baseUrl;
-        $this->refresh();
-    }
-
-    /**
-     * Get actual URL and split it to parameters.
-     */
-    public function refresh()
-    {
-        $baseUrl = str_replace("\/", "/", $this->baseUrl);
-        $baseUrl = str_replace("http://", "", $baseUrl);
-        $baseUrl = str_replace("https://", "", $baseUrl);
-        $uri = str_replace($baseUrl, "", $_SERVER["HTTP_HOST"] .  $_SERVER["REQUEST_URI"]);
-        $this->param = explode("/", $uri);
-        foreach ($this->param as $key => $value) {
-            if (strpos($value, "?") !== false) {
-                $value = substr($value, 0, strpos($value, "?"));
-            }
-            $this->param[$key] = urldecode($value);
+        $this->appUrl = rtrim($appUrl, "/");
+        $this->baseUrl = rtrim(parse_url($this->appUrl, PHP_URL_PATH) ?? "", "/");
+        $requestUri = $_SERVER["REQUEST_URI"] ?? "/";
+        if (
+            $this->baseUrl !== "" &&
+            (
+                $requestUri === $this->baseUrl ||
+                strpos($requestUri, $this->baseUrl . "/") === 0
+            )
+        ) {
+            $this->uri = substr($requestUri, strlen($this->baseUrl));
+        } else {
+            $this->uri = $requestUri;
         }
+        if ($this->uri === "") {
+            $this->uri = "/";
+        }
+        $this->fullUrl = rtrim($this->appUrl, "/");
+        if ($this->uri != "/") {
+            $this->fullUrl .= $this->uri;
+        }
+        $this->path = parse_url($this->uri, PHP_URL_PATH) ?? "/";
+        $this->params = array_values(
+            array_filter(
+                explode("/", trim($this->path, "/")),
+                "strlen"
+            )
+        );
     }
 
-    /**
-     * Return URL parameter.
-     *
-     * @param  int    $key
-     * @return string|null
-     */
-    public function getParam(int $key): ?string
+    public function getAppUrl(): string
     {
-        return $this->param[$key] ?? "";
-    }
-
-    /**
-     * Return full URI.
-     */
-    public function getFullUrl(): string
-    {
-        $toCut = str_replace("\/", "/", $this->baseUrl);
-        $toCut = str_replace("http://", "", $toCut);
-        $toCut = str_replace("https://", "", $toCut);
-        $rest = str_replace($toCut, "", $_SERVER["HTTP_HOST"] .  $_SERVER["REQUEST_URI"]);
-        return "{$this->baseUrl}{$rest}";
+        return $this->appUrl;
     }
 
     public function getBaseUrl(): string
     {
         return $this->baseUrl;
+    }
+
+    public function getFullUrl(): string
+    {
+        return $this->fullUrl;
+    }
+
+    /**
+     * Returns URI relative to base URL.
+     *
+     * Example:
+     * /users/15?page=2
+     */
+    public function getUri(): string
+    {
+        return $this->uri;
+    }
+
+    /**
+     * Returns path without query string.
+     *
+     * Example:
+     * /users/15
+     */
+    public function getPath(): string
+    {
+        return $this->path;
+    }
+
+    /**
+     * Returns all path parameters.
+     */
+    public function getParams(): array
+    {
+        return $this->params;
+    }
+
+    /**
+     * Returns one path parameter or empty string.
+     */
+    public function getParam(int $index): ?string
+    {
+        return $this->params[$index] ?? null;
+    }
+
+    /**
+     * Returns number of path parameters.
+     */
+    public function countParams(): int
+    {
+        return count($this->params);
     }
 }
--- tests/UrlTest.php
+++ tests/UrlTest.php
@@ -6,32 +6,94 @@ use PHPUnit\Framework\TestCase;
 
 class UrlTest extends TestCase
 {
-    private $url;
+    private Url $url;
+    private Url $url2;
+    private Url $url3;
+    private Url $url4;
 
     protected function setUp(): void
     {
-        $_SERVER["HTTP_HOST"] = "pietraspawel.pl";
         $_SERVER["REQUEST_URI"] = "/testowo/param1/param2/param3?aaa=1&bbb=dupa";
         $this->url = new \pietras\basic\Url("http://pietraspawel.pl/testowo");
+
+        $_SERVER["REQUEST_URI"] = "/param1/param2/param3?aaa=1&bbb=dupa";
+        $this->url2 = new \pietras\basic\Url("http://pietraspawel.pl");
+
+        $_SERVER["REQUEST_URI"] = "/testowo";
+        $this->url3 = new \pietras\basic\Url("http://pietraspawel.pl/testowo");
+
+        $_SERVER["REQUEST_URI"] = "/project-old/test";
+        $this->url4 = new \pietras\basic\Url("http://pietraspawel.pl/project");
     }
 
-    public function testUrl()
+    public function testAppUrl()
+    {
+        $this->assertEquals("http://pietraspawel.pl/testowo", $this->url->getAppUrl());
+        $this->assertEquals("http://pietraspawel.pl", $this->url2->getAppUrl());
+        $this->assertEquals("http://pietraspawel.pl/testowo", $this->url3->getAppUrl());
+        $this->assertEquals("http://pietraspawel.pl/project", $this->url4->getAppUrl());
+    }
+
+    public function testBaseUrl()
+    {
+        $this->assertEquals("/testowo", $this->url->getBaseUrl());
+        $this->assertEquals("", $this->url2->getBaseUrl());
+        $this->assertEquals("/testowo", $this->url3->getBaseUrl());
+        $this->assertEquals("/project", $this->url4->getBaseUrl());
+    }
+
+    public function testFullUrl()
     {
         $this->assertEquals(
             "http://pietraspawel.pl/testowo/param1/param2/param3?aaa=1&bbb=dupa",
             $this->url->getFullUrl()
         );
+        $this->assertEquals(
+            "http://pietraspawel.pl/param1/param2/param3?aaa=1&bbb=dupa",
+            $this->url2->getFullUrl()
+        );
+        $this->assertEquals("http://pietraspawel.pl/testowo", $this->url3->getFullUrl());
+        $this->assertEquals("http://pietraspawel.pl/project/project-old/test", $this->url4->getFullUrl());
     }
 
-    public function testBasicUrl()
+    public function testUri()
     {
-        $this->assertEquals("http://pietraspawel.pl/testowo", $this->url->getBaseUrl());
+        $this->assertEquals("/param1/param2/param3?aaa=1&bbb=dupa", $this->url->getUri());
+        $this->assertEquals("/param1/param2/param3?aaa=1&bbb=dupa", $this->url2->getUri());
+        $this->assertEquals("/", $this->url3->getUri());
+        $this->assertEquals("/project-old/test", $this->url4->getUri());
+    }
+
+    public function testPath()
+    {
+        $this->assertEquals("/param1/param2/param3", $this->url->getPath());
+        $this->assertEquals("/param1/param2/param3", $this->url2->getPath());
+        $this->assertEquals("/", $this->url3->getPath());
+        $this->assertEquals("/project-old/test", $this->url4->getPath());
     }
 
     public function testParams()
     {
-        $this->assertEquals("param1", $this->url->getParam(1));
-        $this->assertEquals("param2", $this->url->getParam(2));
-        $this->assertEquals("param3", $this->url->getParam(3));
+        $array = [ "param1", "param2", "param3" ];
+        $this->assertEquals($array, $this->url->getParams());
+        $this->assertEquals("param1", $this->url->getParam(0));
+        $this->assertEquals("param2", $this->url->getParam(1));
+        $this->assertEquals("param3", $this->url->getParam(2));
+        $this->assertEquals(3, $this->url->countParams());
+        $this->assertEquals($array, $this->url2->getParams());
+        $this->assertEquals("param1", $this->url2->getParam(0));
+        $this->assertEquals("param2", $this->url2->getParam(1));
+        $this->assertEquals("param3", $this->url2->getParam(2));
+        $this->assertEquals(3, $this->url2->countParams());
+        $this->assertEquals([], $this->url3->getParams());
+        $this->assertEquals(null, $this->url3->getParam(0));
+        $this->assertEquals(null, $this->url3->getParam(1));
+        $this->assertEquals(null, $this->url3->getParam(2));
+        $this->assertEquals(0, $this->url3->countParams());
+        $this->assertEquals(["project-old", "test"], $this->url4->getParams());
+        $this->assertEquals("project-old", $this->url4->getParam(0));
+        $this->assertEquals("test", $this->url4->getParam(1));
+        $this->assertEquals(null, $this->url4->getParam(2));
+        $this->assertEquals(2, $this->url4->countParams());
     }
 }
